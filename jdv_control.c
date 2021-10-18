b7975b9f96324f5fa9233c0d39139a7f76028651
RVCoelho <80905108+RVCoelho@users.noreply.github.com>
2021-10-17 22:37:26 -0300

    Update jdv_control.c

--- jdv_control.c
+++ jdv_control.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
-#include "JVIDA-JJVR-Model.c"
-#include "JVIDA-JJVR-View.c"
+#include "JVIDA_JJVR_Model.c"
+#include "JVIDA_JJVR_View.c"
 
 void jogoDaVida();
-
+void geraMundo();
+void apresentaMundo();
+void selecionaVivos();
+void limpaMundo();
 
 void jogoDaVida()
 {
@@ -18,6 +20,7 @@ void jogoDaVida()
                 break;
 
             case 2:
+                selecionaVivos();
                 break;
 
             case 3:
@@ -27,9 +30,7 @@ void jogoDaVida()
                 break;
 
             case 5:
-                break;
-
-            case 6:
+                limpaMundo();
                 break;
 
             case 0:
@@ -39,25 +40,84 @@ void jogoDaVida()
     }
 }
 
-void geraMundo()
+void apresentaMundo()
 {
-    int i, k, tam, j=0;
+    int i, k;
 
-    printf("Escolha o tamanho do mundo (maximo: 100): ");
-    scanf("%d", &tam);
-
-    for(i=0;i<=tam;i++)
-        printf("%2.d", i);
-
-    for(i=0;i<=tam;i++)
+    printf("\t");
+    for(i=1;i<=mundo.tam;i++)
+        printf("%d\t", i);
+    printf("\n");
+    
+    for(i=1;i<=mundo.tam;i++)
     {
-        printf("%2.d", k);
-
-        for(k=0;k<=tam;k++)
+        printf("%d\t", i);
+        for(k=0;k<mundo.tam;k++)
         {
-            printf(".\t");
-
+            if(mundo.status[i][k]=='M') //morto
+                printf(".\t");
+            else if(mundo.status[i][k]=='V') //vivo
+                printf("o\t");
         }
         printf("\n");
+    }
+}
+
+
+void geraMundo()
+{
+    int i, k;
+
+    do
+    {
+        printf("Escolha o tamanho do mundo (maximo: 100): ");
+        scanf("%d", &mundo.tam);
+    }while(mundo.tam<1 || mundo.tam>100);
+
+    for(i=1;i<=mundo.tam;i++)
+    {
+        for(k=0;k<mundo.tam;k++)
+            mundo.status[i][k]='M';
+    }
+    apresentaMundo();
+}
+
+void selecionaVivos()
+{
+    int vivasInicio, aux1, aux2, i, k;
+
+	do
+    {
+        printf("Quantas celulas vivas voce vai querer colocar no mundo?: ");
+	    scanf("%d",&vivasInicio);
+    }while(vivasInicio<1);
+	printf("\n");
+
+	for(i=1;i<=vivasInicio;i++)
+    {
+        aux1=0;
+        aux2=0;
+        printf("Selecione a coordenada da celula viva #%d: ",i);
+        scanf("%d %d",&aux1, &aux2);
+        mundo.status[aux1][aux2-1]='V';
+        printf("\n");
+    }
+    apresentaMundo();
+}
+
+void limpaMundo()
+{
+    int aux, i, j;
+
+    printf("Voce quer limpar o mundo? Sim:1 / Nao:2\n");
+	scanf("%d",&aux);
+	if(aux==1)
+    {
+        for(i=0;i<=mundo.tam;i++)
+        {
+            for(j=0;j<=mundo.tam;j++)
+                mundo.status[i][j] = 'M';
+        }
+        apresentaMundo();
     }
 }
